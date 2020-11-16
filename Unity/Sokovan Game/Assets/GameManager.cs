5802e988a532a8ba374a526bdbef14a6a3b1960f
Jangdawit <jangdawit@enha.edu>
2020-11-16 20:08:34 +0900

    my first unity game

---
+++ Unity/Sokovan Game/Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject winUI;
+    public ItemBox[] boxes;
+
+    public bool isGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("main");
+        }
+
+        int count = 0;
+        if (isGameOver)
+        {
+            return;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (boxes[i].isOveraped == true)
+            {
+                count++;
+            }
+        }
+
+        if (count >= 3)
+        {
+            isGameOver = true;
+            winUI.SetActive(true);
+        }
+    }
+}
---
+++ Unity/Sokovan Game/Assets/ItemBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    private Renderer myRenderer;
+
+    public bool isOveraped = false;
+
+    public Color touchColor;
+    private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            isOveraped = false;
+            myRenderer.material.color = originalColor;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+        }
+    }
+}
