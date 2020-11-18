97dfe9e78759b6d5b5aa21b010787305f79e9b8a
Jangdawit <jangdawit@enha.edu>
2020-11-19 05:04:24 +0900

    comparison operators for PHP

---
+++ PHP/compare/compare2.php
@@ -0,0 +1,16 @@
+<?php
+# 비교 연산자(!=)의 사용
+
+echo "1!=2 : ";
+var_dump(1!=2);           #true
+echo '<br />';
+echo "1!=1 : ";
+var_dump(1!==1);           #false
+echo '<br />';
+echo '"one"!="two" : ';
+var_dump("one"!="two");   #true
+echo '<br />';
+echo '"one"!="one" : ';
+var_dump("one"!="one");   #false
+echo '<br />';
+?>
---
+++ PHP/compare/compare3.php
@@ -0,0 +1,12 @@
+<?php
+# 비교 연산자(>) 의 사용
+
+echo "10>20 : ";
+var_dump(10>20);           #false
+echo '<br />';
+echo "10>1 : ";
+var_dump(10>1);           #true
+echo '<br />';
+echo '10>10 : ';
+var_dump(10>10);   #false
+?>
